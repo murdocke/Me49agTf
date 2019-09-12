8beb29c3c3c6e61b76af168646f775a9061055ff
Jason Stillwell <jasonstillwell@Jasons-MacBook-Pro.local>
2019-09-12 16:52:21 -0500

    added comments in route file

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,15 @@
 |
 */
 
+//this should probably end up in api route file as it is used for dyamic jquery make/model dropdowns
 Route::get('/getModels/{id}', 'ServiceRequestsController@getModels');
 
 Route::get('/', 'ServiceRequestsController@index')->name('home');
+
 Route::get('/create', 'ServiceRequestsController@create')->name('create');
 Route::post('/store', 'ServiceRequestsController@store')->name('doCreate');
+
+//changed from dynamic portion from id to serviceRequest to make route model binding work
 Route::get('{serviceRequest}', 'ServiceRequestsController@edit')->name('edit');
 Route::post('/update', 'ServiceRequestsController@update')->name('doEdit');
 Route::delete('/','ServiceRequestsController@destroy')->name('doDelete');
